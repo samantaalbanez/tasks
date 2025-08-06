c2890ab35237c6dcce59fce0e4c77280de124246
Samanta Albanez <samantaalbanez@MacBook-Pro-de-Samanta.local>
2025-08-05 21:31:56 -0300

    lints

--- app/src/main/java/com/samantaalbanez/mytasks/presentation/ui/screen/AddTasksScreen.kt
+++ app/src/main/java/com/samantaalbanez/mytasks/presentation/ui/screen/AddTasksScreen.kt
@@ -9,14 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
